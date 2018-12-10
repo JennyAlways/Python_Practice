ef38fb6a0b6e4b39eb7fad58d00bfc73fd59df0c
jenny always <1179268224@qq.com>
2018-12-10 20:10:36 +0800

    Py20181203

---
+++ Py20181203/Classwork1.py
@@ -0,0 +1,21 @@
+
+import turtle
+import time
+
+t=turtle.Pen()
+
+t.goto(0,100)
+t.goto(400,100)
+t.goto(400,200)
+t.goto(600,0)
+t.penup()
+t.goto(400,-200)
+t.pendown()
+t.goto(400,-100)
+t.goto(0,-100)
+t.goto(0,0)
+
+turtle.done()
+
+#penup
+#pendown
