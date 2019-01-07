61f656ce07b3d1fbbaf481b1ddf36c63a3d02e9c
jenny always <1179268224@qq.com>
2019-01-07 19:57:41 +0800

    Hello:
    Hello

---
+++ Py20190107/Dialog.py
@@ -0,0 +1,22 @@
+name= raw_input ("Hello, this is Jenny. You are...\n")
+
+age= raw_input ("Nice to meet you, " + name + "!" + " How old are you?\n")
+
+sexual= raw_input ("Ok, so are you a female or a male?\n")
+
+sports= raw_input ("Great! Exercise makes you healthy. My friend have been running " + str(30) + " minutes everyday! Do you have a favourite sport?\n")
+
+happiness= raw_input("On a scale of one(little) to ten(a lot), how would you rate your daily happiness?\n")
+
+Help= raw_input ("Well, if you have any problems, I will always be here to listen for you! Actually, I might help you solve your problems by searching ideas! Now, please tell me about your doubts.\n")
+
+goodbye= raw_input ("You know what, you are really not that talktive online, I hope we could share more! Anyway, nice to meet you and see you later, if possible. Here is your report.\n")
+
+print ("Your name is " +name+ ". You are " + age+ " years old. You are a " + sexual + " who likes to play/do/etc. " + sports + ". You think that you have the happiness of " + happiness + " out of ten. Your problem is that " + "'"+ Help + "'" +" Enjoy life!\n")
+
+
+
+             
+
+
+
